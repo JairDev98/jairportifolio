f99daad08b8d9ec9cb5b9ffebf24ae4bb2942aed
Jair Aparecido de Oliveira <jair.estu19@gmail.com>
2021-09-07 21:56:05 -0300

    07_09_21

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document{
                 <link rel="preconnect" href="https://fonts.gstatic.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet"/>
                 <link rel="icon" href="icons/favicon.ico" />
+                <title>SyDJO</title>
                 </Head>
                 <body>
                     <Main />
